0749acd469859b7f6039df60de15b589ca9f9bc2
humichen <43415952+humichen@users.noreply.github.com>
2019-05-08 11:29:16 +0800

    Add files via upload

---
+++ extra button edgetrigger/sketch_may08b.ino
@@ -0,0 +1,19 @@
+int ledpin=13;
+int btnpin=2;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledpin,OUTPUT);
+pinMode(btnpin,INPUT);
+//digitalWrite(btnpin,HIGH);
+}
+int btnstate=0;
+boolean ledstate=false;
+void loop() {
+  // put your main code here, to run repeatedly:
+btnstate=digitalRead(btnpin);
+if(btnstate==HIGH){
+  ledstate = !ledstate;
+  digitalWrite(ledpin,ledstate);
+  delay(200);
+}
+}
