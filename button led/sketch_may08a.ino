07283968e22d4737dd9d2e8dcc5f16215a2caf77
humichen <43415952+humichen@users.noreply.github.com>
2019-05-08 11:27:57 +0800

    Add files via upload

---
+++ button led/sketch_may08a.ino
@@ -0,0 +1,19 @@
+int ledpin=13;
+int btnpin=2;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledpin,OUTPUT);
+pinMode(btnpin,INPUT);
+//digitalWrite(btnpin,HIGH);
+}
+int btnstate=0;
+void loop() {
+  // put your main code here, to run repeatedly:
+btnstate=digitalRead(btnpin);
+if(btnstate==HIGH){
+  digitalWrite(ledpin,HIGH);
+  }
+ else{
+   digitalWrite(ledpin,LOW);
+  }
+}
