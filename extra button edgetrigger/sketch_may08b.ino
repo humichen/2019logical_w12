25a096af5d512282cb32f23577756066724b8118
humichen <43415952+humichen@users.noreply.github.com>
2019-05-08 11:54:11 +0800

    Add files via upload

--- extra button edgetrigger/sketch_may08b.ino
+++ extra button edgetrigger/sketch_may08b.ino
@@ -1,19 +1,23 @@
 int ledpin=13;
 int btnpin=2;
+int btnstate=0;
+int oldbtn=0;
+boolean ledstate=false;
+
 void setup() {
   // put your setup code here, to run once:
 pinMode(ledpin,OUTPUT);
 pinMode(btnpin,INPUT);
 //digitalWrite(btnpin,HIGH);
 }
-int btnstate=0;
-boolean ledstate=false;
+
 void loop() {
   // put your main code here, to run repeatedly:
 btnstate=digitalRead(btnpin);
-if(btnstate==HIGH){
+if(btnstate==HIGH && oldbtn==0){
   ledstate = !ledstate;
   digitalWrite(ledpin,ledstate);
-  delay(200);
 }
+oldbtn=btnstate;
+delay(20);
 }
